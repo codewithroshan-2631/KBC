9ae895b1708f9ed1c2134a1feb102b1e7a669194
Roshan Manjhi <roshanmanjhi2631@gmail.com>
2025-07-23 19:32:08 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,27 @@
+#Snake water and gun Game
+
+import random
+def check(comp,user):
+    if comp==user:
+        return 0
+    if(comp==0 and user==1):
+        retutn -1
+    if(comp==1 and user==2):
+        return -1
+    if(comp==2 and user==0):
+        return -1
+    return 1
+
+comp=random.randint(0,2)
+user=int(input("0 for Snake,1 for Water and 2 for Gun:\n"))
+
+score=check(comp,user)
+print("You:",user)
+print("Computer:",comp)
+
+if(score==0):
+    print("It's draw match")
+elif(score==-1):
+    print("You Lose")
+else:
+    print("You Won")    
